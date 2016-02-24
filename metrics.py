d7c257dec7c254405417a5aabd310368e9c64f7f
John Ladan <john@ladan.ca>
2016-02-23 20:06:28 -0500

    initial commit

---
+++ metrics.py
@@ -0,0 +1,29 @@
+"""
+A collection of different norms that work on finite-dimensional collections of numbers
+"""
+
+from numpy import array
+from math import sqrt
+
+def l1(x_in, y_in):
+    x = array(x_in).flatten()
+    y = array(y_in).flatten()
+    return sum(abs(x-y))
+
+def l2(x_in, y_in):
+    x = array(x_in).flatten()
+    y = array(y_in).flatten()
+    return sqrt(sum((x-y)**2))
+
+def sup(x_in, y_in):
+    x = array(x_in).flatten()
+    y = array(y_in).flatten()
+    return max(abs(x-y))
+
+def manhattan(x_in, y_in):
+    x = array(x_in).flatten()
+    y = array(y_in).flatten()
+    # for some reason, `-` is evaluated as component-wise xor
+    difference = (abs(x) >=1 ) - (abs(y) >=1 )
+    print(difference)
+    return sum( difference * 1)
---
+++ norms.py
@@ -0,0 +1,22 @@
+"""
+A collection of different norms that work on finite-dimensional collections of numbers
+"""
+
+from numpy import array
+from math import sqrt
+
+def l1(x_in):
+    x = array(x_in).flatten()
+    return sum(abs(x))
+
+def l2(x_in):
+    x = array(x_in).flatten()
+    return sqrt(sum(x**2))
+
+def sup(x_in):
+    x = array(x_in).flatten()
+    return max(abs(x))
+
+def manhattan(x_in):
+    x = array(x_in).flatten()
+    return sum( ( abs(x) >= 1) * 1 )
