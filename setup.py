11fd639fe0e8eec49972dd96a206b385269409ea
John Ladan <john@ladan.ca>
2016-02-24 17:13:49 -0500

    a working setup.py file now

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
 from setuptools import setup
 
-setup()
+setup(
+    # Needed to silence warnings
+    name='Measurements',
+    url='https://github.com/jladan/package_demo',
+    author='John Ladan',
+    author_email='jladan@uwaterloo.ca',
+    # Needed to actually package something
+    packages=['measure'],
+    # Needed for dependencies
+    install_requires=['numpy'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
